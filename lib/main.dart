23c500d5e46042e864d2fa55af300ecf60227491
torder-raven <160463057+torder-raven@users.noreply.github.com>
2024-03-14 08:32:41 +0900

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:calendar_scheduler/di/locator.dart';
-import 'package:calendar_scheduler/domain/usecase/register_schedule.dart';
 import 'package:calendar_scheduler/presentation/const/theme.dart';
 import 'package:calendar_scheduler/presentation/screen/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-
-import 'domain/entity/schedule.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
