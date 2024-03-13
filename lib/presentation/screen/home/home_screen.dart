b6c978e23153d661d91f638d5e4ae9370a8eb051
ben-jhkim <kjh8@torder.com>
2024-03-13 12:47:10 +0900

    feature: add calendar
    - 캘린더 추가
    - 토요일, 일요일 색 변경

--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:calendar_scheduler/presentation/screen/component/calendar.dart';
+import 'package:flutter/material.dart';
+
+final _FIRST_DAY = DateTime.utc(1900);
+final _LAST_DAY = DateTime.utc(3000);
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +12,53 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DateTime _selectedDay = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Calendar(
+              focusedDay: _selectedDay,
+              firstDay: _FIRST_DAY,
+              lastDay: _LAST_DAY,
+              selectedDay: _selectedDay,
+              onDaySelected: onDaySelected,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  DateTime today() {
+    final now = DateTime.now();
+    return DateTime.utc(
+      now.year,
+      now.month,
+      now.day,
+    );
+  }
+
+  void onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      _selectedDay = selectedDay;
+    });
   }
 }
