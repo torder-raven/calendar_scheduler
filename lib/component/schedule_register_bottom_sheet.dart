27476dfbd3565330b9920db50537762fccc20c14
kite-torder <jjy5@torder.com>
2024-03-13 11:01:14 +0900

    feat: 바텀시트 상단 header 뷰 배치 완료

---
+++ lib/component/schedule_register_bottom_sheet.dart
@@ -0,0 +1,55 @@
+import 'package:calendar_scheduler/component/color_selection_field.dart';
+import 'package:flutter/material.dart';
+
+class ScheduleRegisterBottomSheet extends StatelessWidget {
+  final DateTime selectedDate;
+
+  const ScheduleRegisterBottomSheet({super.key, required this.selectedDate});
+
+  @override
+  Widget build(BuildContext context) {
+    final bottomInset = MediaQuery.of(context).viewInsets.bottom;
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height / 2 + bottomInset,
+        child: Padding(
+          padding: EdgeInsets.only(bottom: bottomInset),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _BottomSheetHeader(
+                selectedDate: selectedDate,
+              ),
+              ColorSelectionField(colorCode: 0)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _BottomSheetHeader extends StatelessWidget {
+  final DateTime selectedDate;
+
+  const _BottomSheetHeader({super.key, required this.selectedDate});
+
+  @override
+  Widget build(BuildContext context) {
+    String title =
+        "${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일";
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      Text(
+        title,
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w800),
+      ),
+      IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.close),
+      ),
+    ]);
+  }
+}
