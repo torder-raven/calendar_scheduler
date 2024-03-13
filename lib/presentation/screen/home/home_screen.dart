eec8983a88f294427db95649cd4dbed41a73e9c3
ben-jhkim <kjh8@torder.com>
2024-03-13 15:55:43 +0900

    refactor: add extension toUtcDate

--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_scheduler/presentation/extension.dart';
 import 'package:calendar_scheduler/presentation/screen/component/calendar.dart';
 import 'package:flutter/material.dart';
 
@@ -12,11 +13,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  DateTime _selectedDay = DateTime.utc(
-    DateTime.now().year,
-    DateTime.now().month,
-    DateTime.now().day,
-  );
+  DateTime _selectedDay = DateTime.now().toUtcDate();
 
   @override
   void initState() {
